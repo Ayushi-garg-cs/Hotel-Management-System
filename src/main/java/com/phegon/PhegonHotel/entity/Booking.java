efc91f4712cfa07dc036e9e72f4a1b20ebab164b
Ayushi-garg-cs <ayushigarg2346@gmail.com>
2026-04-17 21:37:57 +0530

    Initial commit

---
+++ src/main/java/com/phegon/PhegonHotel/entity/Booking.java
@@ -0,0 +1,11 @@
+package com.phegon.PhegonHotel.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name="bookings")
+public class Booking {
+}
